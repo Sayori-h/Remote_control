d9fcdde4bd0d322b12ee32bca890dd62768a4c95
Sayori-h <3186385448@qq.com>
2023-08-30 08:49:42 +0800

    1完成了recv的处理

--- RemoteCtrl/RemoteCtrl/HuxlServer.cpp
+++ RemoteCtrl/RemoteCtrl/HuxlServer.cpp
@@ -21,14 +21,41 @@ int AcceptOverlapped<op>::AcceptWorker() {
 			(sockaddr**)m_client->GetLocalAddr(), &local_length,//本地地址
 			(sockaddr**)m_client->GetRemoteAddr(), &remote_length//远程地址
 		);
-		PCLIENT pClient(new HuxlClient());//所有的人拿到的都是引用，一个shared_ptr
-		m_server->m_client.insert(std::pair<SOCKET, PCLIENT>(*pClient, pClient));
+		//PCLIENT pClient(new HuxlClient());//所有的人拿到的都是引用，一个shared_ptr
+		//m_server->m_client.insert(std::pair<SOCKET, PCLIENT>(*pClient, pClient));
+        
+		int ret = WSARecv((SOCKET)*m_client, m_client->RecvWSABuffer(),
+			1, *m_client, &m_client->flags(), *m_client, NULL);
+		if (ret == SOCKET_ERROR && (WSAGetLastError() != WSA_IO_PENDING)) {
+			//TODO:报错
+		}
 		if (!m_server->NewAccept()) return -2;
 	}
 	return -1;
 }
 
-HuxlClient::HuxlClient() :m_isbusy(false), m_overlapped(new ACCEPTOVERLAPPED()) {
+template<HuxlOperator op>SendOverlapped<op>::SendOverlapped()
+{
+    m_operator = op;
+    m_worker = ThreadWorker(this, (FUNCTYPE)&SendOverlapped<op>::SendWorker);
+    memset(&m_overlapped, 0, sizeof(m_overlapped));
+    m_buffer.resize(1024 * 256);
+}
+
+template<HuxlOperator op>RecvOverlapped<op>::RecvOverlapped()
+{
+    m_operator = op;
+    m_worker = ThreadWorker(this, (FUNCTYPE)&RecvOverlapped<op>::RecvWorker);
+    memset(&m_overlapped, 0, sizeof(m_overlapped));
+    m_buffer.resize(1024 * 256);
+}
+
+HuxlClient::HuxlClient()
+	:m_isbusy(false), m_flags(0),
+    m_overlapped(new ACCEPTOVERLAPPED()),
+    m_recv(new RECVOVERLAPPED()),
+    m_send(new SENDOVERLAPPED())
+{
 	m_sock = WSASocket(PF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
 	m_buffer.resize(1024);
 	memset(&m_laddr, 0, sizeof(m_laddr));
@@ -37,8 +64,88 @@ HuxlClient::HuxlClient() :m_isbusy(false), m_overlapped(new ACCEPTOVERLAPPED()) 
 
 void HuxlClient::SetOverlapped(PCLIENT& ptr) {
 	m_overlapped->m_client = ptr;
+    m_recv->m_client = ptr;
+    m_send->m_client = ptr;
 }
 
 HuxlClient::operator LPOVERLAPPED() {
 	return &m_overlapped->m_overlapped;
+}
+
+LPWSABUF HuxlClient::RecvWSABuffer()
+{
+    return &m_recv->m_wsabuffer;
+}
+
+LPWSABUF HuxlClient::SendWSABuffer()
+{
+    return &m_send->m_wsabuffer;
+}
+
+bool HuxlServer::StartService()
+{
+    CreateSocket();
+    if (bind(m_sock, (sockaddr*)&m_addr, sizeof(m_addr)) == -1) {
+        closesocket(m_sock);
+        m_sock = INVALID_SOCKET;
+    }
+    if (listen(m_sock, 3) == -1) {
+        closesocket(m_sock);
+        m_sock = INVALID_SOCKET;
+        return false;
+    }
+    m_hIOCP = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 4);
+    if (m_hIOCP == NULL) {
+        closesocket(m_sock);
+        m_sock = INVALID_SOCKET;
+        m_hIOCP = INVALID_HANDLE_VALUE;
+        return false;
+    }
+    CreateIoCompletionPort((HANDLE)m_sock, m_hIOCP, (ULONG_PTR)this, 0);
+    m_pool.Invoke();
+    m_pool.DispatchWoker(ThreadWorker(this, (FUNCTYPE)&HuxlServer::threadIocp));
+    if (!NewAccept())return false;
+    //m_pool.DispatchWoker(ThreadWorker(this, (FUNCTYPE)&HuxlServer::threadIocp));
+    //m_pool.DispatchWoker(ThreadWorker(this, (FUNCTYPE)&HuxlServer::threadIocp));
+    return true;
+}
+
+int HuxlServer::threadIocp()
+{
+    DWORD tranferred = 0;
+    ULONG_PTR CompletionKey = 0;
+    OVERLAPPED* lpOverlapped = NULL;
+    if (GetQueuedCompletionStatus(m_hIOCP, &tranferred, &CompletionKey, &lpOverlapped, INFINITE)) {
+        if (tranferred > 0 && (CompletionKey != 0)) {
+            HuxlOverlapped* pOverlapped = CONTAINING_RECORD(lpOverlapped, HuxlOverlapped, m_overlapped);
+            switch (pOverlapped->m_operator) {
+            case EAccept:
+            {
+                ACCEPTOVERLAPPED* pOver = (ACCEPTOVERLAPPED*)lpOverlapped;
+                m_pool.DispatchWoker(pOver->m_worker);
+            }
+            break;
+            case ERecv:
+            {
+                RECVOVERLAPPED* pOver = (RECVOVERLAPPED*)lpOverlapped;
+                m_pool.DispatchWoker(pOver->m_worker);
+            }
+            break;
+            case ESend:
+            {
+                SENDOVERLAPPED* pOver = (SENDOVERLAPPED*)lpOverlapped;
+                m_pool.DispatchWoker(pOver->m_worker);
+            }
+            break;
+            case EError:
+            {
+                ERROROVERLAPPED* pOver = (ERROROVERLAPPED*)lpOverlapped;
+                m_pool.DispatchWoker(pOver->m_worker);
+            }
+            break;
+            }
+        }
+        else return -1;
+    }
+    return 0;
 }
--- RemoteCtrl/RemoteCtrl/HuxlServer.h
+++ RemoteCtrl/RemoteCtrl/HuxlServer.h
@@ -24,11 +24,19 @@ public:
     std::vector<char> m_buffer;//缓冲区
     ThreadWorker m_worker;//处理函数
     HuxlServer* m_server;//服务器对象
+    PCLIENT m_client;//对应的客户端
+    WSABUF m_wsabuffer;
+
 
 };
 
 template<HuxlOperator>class AcceptOverlapped;
 typedef AcceptOverlapped<EAccept> ACCEPTOVERLAPPED;
+template<HuxlOperator>class RecvOverlapped;
+typedef RecvOverlapped<ERecv> RECVOVERLAPPED;
+template<HuxlOperator>class SendOverlapped;
+typedef SendOverlapped<ESend> SENDOVERLAPPED;
+
 
 class HuxlClient {
 public:
@@ -53,13 +61,30 @@ public:
         return &m_received;
     }
 
+    LPWSABUF RecvWSABuffer();
+
+    LPWSABUF SendWSABuffer();
+    DWORD& flags() { return m_flags; }
     sockaddr_in* GetLocalAddr()  { return &m_laddr; }
     sockaddr_in* GetRemoteAddr() { return &m_raddr; }
+    size_t GetBufferSize() const { return m_buffer.size(); }
+
+    int Recv() {
+        int ret=recv(m_sock, m_buffer.data()+m_used, m_buffer.size()-m_used, 0);
+        if (ret <= 0)return -1;
+        m_used += (size_t)ret;
+        //TODO:解析数据
+        return 0;
+    }
 private:
     SOCKET m_sock;
     DWORD m_received;
+    DWORD m_flags;
     std::shared_ptr<ACCEPTOVERLAPPED> m_overlapped;
+    std::shared_ptr<RECVOVERLAPPED>m_recv;
+    std::shared_ptr<SENDOVERLAPPED>m_send;
     std::vector<char> m_buffer;
+    size_t m_used;//已经使用的缓冲区大小
     sockaddr_in m_laddr;
     sockaddr_in m_raddr;
     bool m_isbusy;
@@ -79,12 +104,10 @@ template<HuxlOperator>
 class RecvOverlapped :public HuxlOverlapped, ThreadFuncBase
 {
 public:
-    RecvOverlapped() :m_operator(ERecv), m_worker(this, &RecvOverlapped::RecvWorker) {
-        memset(&m_overlapped, 0, sizeof(m_overlapped));
-        m_buffer.resize(1024*256);
-    }
+    RecvOverlapped();
     int RecvWorker() {
-        //TODO:
+        int ret = m_client->Recv();
+        return ret;
     }
 };
 
@@ -94,12 +117,10 @@ template<HuxlOperator>
 class SendOverlapped :public HuxlOverlapped, ThreadFuncBase
 {
 public:
-    SendOverlapped() :m_operator(ESend), m_worker(this, &SendOverlapped::SendWorker) {
-        memset(&m_overlapped, 0, sizeof(m_overlapped));
-        m_buffer.resize(1024*256);
-    }
+    SendOverlapped();
     int SendWorker() {
         //TODO:
+        return -1;
     }
 };
 
@@ -114,11 +135,10 @@ public:
     }
     int ErrorWorker() {
         //TODO:
+        return -1;
     }
 };
 
-typedef RecvOverlapped<ERecv> RECVOVERLAPPED;
-typedef SendOverlapped<ESend> SENDOVERLAPPED;
 typedef ErrorOverlapped<EError> ERROROVERLAPPED;
 
 
@@ -138,35 +158,11 @@ public:
 
     ~HuxlServer(){}
 
-    bool StartService() {
-        CreateSocket();
-        if (bind(m_sock, (sockaddr*)&m_addr, sizeof(m_addr)) == -1) {
-            closesocket(m_sock);
-            m_sock = INVALID_SOCKET;
-        }
-        if (listen(m_sock, 3) == -1) {
-            closesocket(m_sock);
-            m_sock = INVALID_SOCKET;
-            return false;
-        }
-        m_hIOCP = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 4);
-        if (m_hIOCP == NULL) {
-            closesocket(m_sock);
-            m_sock = INVALID_SOCKET;
-            m_hIOCP = INVALID_HANDLE_VALUE;
-            return false;
-        }
-        CreateIoCompletionPort((HANDLE)m_sock, m_hIOCP, (ULONG_PTR)this, 0);
-        m_pool.Invoke();
-        m_pool.DispatchWoker(ThreadWorker(this, (FUNCTYPE)&HuxlServer::threadIocp));
-        if (!NewAccept())return false;
-        //m_pool.DispatchWoker(ThreadWorker(this, (FUNCTYPE)&HuxlServer::threadIocp));
-        //m_pool.DispatchWoker(ThreadWorker(this, (FUNCTYPE)&HuxlServer::threadIocp));
-        return true;
-    }
+    bool StartService();
 
     bool NewAccept() {
         PCLIENT pClient(new HuxlClient());//所有的人拿到的都是引用，一个shared_ptr
+        pClient->SetOverlapped(pClient);
         m_client.insert(std::pair<SOCKET, PCLIENT>(*pClient, pClient));
         if (!AcceptEx(m_sock, *pClient, *pClient, 0,
             sizeof(sockaddr_in) + 16, sizeof(sockaddr_in) + 16, *pClient, *pClient)) {
@@ -188,44 +184,7 @@ private:
         setsockopt(m_sock, SOL_SOCKET, SO_REUSEADDR, (const char*)&opt, sizeof(opt));
     }
     
-    int threadIocp() {
-        DWORD tranferred = 0;
-        ULONG_PTR CompletionKey = 0;
-        OVERLAPPED* lpOverlapped = NULL;
-        if (GetQueuedCompletionStatus(m_hIOCP, &tranferred, &CompletionKey, &lpOverlapped, INFINITE)) {
-            if (tranferred > 0 && (CompletionKey != 0)) {
-                HuxlOverlapped* pOverlapped = CONTAINING_RECORD(lpOverlapped, HuxlOverlapped, m_overlapped);
-                switch (pOverlapped->m_operator) {
-                case EAccept:
-                {
-                    ACCEPTOVERLAPPED* pOver = (ACCEPTOVERLAPPED*)lpOverlapped;
-                    m_pool.DispatchWoker(pOver->m_worker);
-                }
-                break;
-                case ERecv:
-                {
-                    RECVOVERLAPPED* pOver = (RECVOVERLAPPED*)lpOverlapped;
-                    m_pool.DispatchWoker(pOver->m_worker);
-                }
-                break;
-                case ESend:
-                {
-                    SENDOVERLAPPED* pOver = (SENDOVERLAPPED*)lpOverlapped;
-                    m_pool.DispatchWoker(pOver->m_worker);
-                }
-                break;
-                case EError:
-                {
-                    ERROROVERLAPPED* pOver = (ERROROVERLAPPED*)lpOverlapped;
-                    m_pool.DispatchWoker(pOver->m_worker);
-                }
-                break;
-                }
-            }
-            else return -1;
-        }
-        return 0;
-    }
+    int threadIocp();
 public:
     std::map<SOCKET, std::shared_ptr<HuxlClient>> m_client;
 private:
@@ -237,6 +196,7 @@ private:
     sockaddr_in m_addr;
     CEdoyunQueue<HuxlClient> m_lstClient;
 };
+
 
 
 
